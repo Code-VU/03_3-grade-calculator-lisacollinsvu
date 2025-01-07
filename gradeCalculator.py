8f87d2207c54381acb6795f1a3e74e02c1b06498
Lisa Collins <Lisa.Collins@vu.com>
2025-01-07 14:43:23 -0600

    Lisa grade calc

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -2,8 +2,19 @@ def calculateGrade():
     # Implement your solution in between the two comment blocks
     print("Calculating Grade")
     # This first line is provided for you
-
-    hrs = float(input("Enter score:"))
+    try:
+        inRange = True
+        hrs = float(input("Enter score:"))
+        if hrs<0: inRange = False
+        if hrs>1: inRange = False
+    except: inRange = False
+    if inRange == False: print("Bad score")
+    else:
+        if hrs >= 0.9: print("A")
+        elif hrs >= 0.8: print("B")
+        elif hrs >= 0.7: print("C")
+        elif hrs >= 0.6: print("D")
+        else: print("F")
 
     # end assignment
 
